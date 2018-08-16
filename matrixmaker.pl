85a604bcb5b32060ab41100d831ea701bfbebeb5
Unknown <worldtour@live.de>
2018-08-16 10:57:45 +0200

    test for matrix

--- matrixmaker.pl
+++ matrixmaker.pl
@@ -12,7 +12,7 @@ open(ER,'>>',"/home/daniel/logfile_auto.log")||die "$!";		# global logfile
 #system("clear");
 
 my $start = time;
-chdir "../";
+#chdir "../";
 
 my$linfile= $ARGV[0];
 chomp $linfile;
